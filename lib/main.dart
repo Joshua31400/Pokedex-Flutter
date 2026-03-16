a8bc7da614e41fbb5ac5bf4998e63d0dc9bacb18
Pedro Martins <pedrohenrique.costamartins@ynov.com>
2026-03-16 10:55:32 +0100

    hotfix:
    - removed local class tester from main

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pokemon App',
       navigatorKey: navigatorKey,
-      home: Tester(),
+      home: Placeholder(),
     );
   }
 }
